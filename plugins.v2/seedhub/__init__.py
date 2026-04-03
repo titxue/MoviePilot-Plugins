16fa08f7fe907aaf0265a08c1ac9a235ee8ba934
xuejy <xue283866@gmail.com>
2026-04-03 11:43:02 +0800

    feat(seedhub): 添加 SeedHub 搜索插件实现
    - 实现了 SeedHub 影视资源搜索功能
    - 集成了夸克等下载链接提取服务
    - 提供了完整的页面交互界面
    - 支持搜索结果和链接获取操作
    - 添加了历史记录管理功能
    - 实现了 API 接口和命令行支持
    - 配置了插件的基本信息和权限设置

--- plugins.v2/seedhub/__init__.py
+++ plugins.v2/seedhub/__init__.py
@@ -25,7 +25,7 @@ class SeedHub(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/Hqyel/MoviePilot-Plugins/main/icons/nullbr.png"
     # 插件版本
-    plugin_version = "1.3.1"
+    plugin_version = "1.3.2"
     # 插件作者
     plugin_author = "Claude"
     # 作者主页
